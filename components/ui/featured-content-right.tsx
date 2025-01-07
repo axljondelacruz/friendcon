d456cf2a327f345e1321ab08af3af674f643e44c
Axl Jon Dela Cruz <adelacruz@auditboard.com>
2025-01-06 23:05:12 -0800

    Fix type errors

--- components/ui/featured-content-right.tsx
+++ components/ui/featured-content-right.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 
 import Cta from '@/components/ui/cta'
 
@@ -17,10 +17,10 @@ export default function FeaturedContentRight({
 }: {
   children?: React.ReactNode
   className?: string
-  header: string
-  body: string
+  header?: string
+  body?: string | TrustedHTML
   id?: string
-  imageSrc?: string
+  imageSrc?: StaticImageData
   imageAlt?: string
   linkUrl1?: string
   cta1?: string
@@ -34,19 +34,21 @@ export default function FeaturedContentRight({
       id={id}
       className={`featured-content-right p-4 flex-1 ${order} ${className}`}
     >
-      {imageSrc ? (
+      {imageSrc && imageAlt ? (
         <Image className="rounded-full" src={imageSrc} alt={imageAlt} />
       ) : (
         <>
           <h2 className="h3 text-black">{header}</h2>
           <div className="featured-content-body mt-4">
-            <p
-              className="text-black"
-              dangerouslySetInnerHTML={{ __html: body }}
-            />
+            {body && (
+              <p
+                className="text-black"
+                dangerouslySetInnerHTML={{ __html: body }}
+              />
+            )}
           </div>
           <div className="flex flex-row space-x-2 justify-center">
-            <Cta linkUrl={linkUrl1}>{cta1}</Cta>
+            {linkUrl1 && cta1 && <Cta linkUrl={linkUrl1}>{cta1}</Cta>}
             {linkUrl2 && cta2 && <Cta linkUrl={linkUrl2}>{cta2}</Cta>}
           </div>
         </>
